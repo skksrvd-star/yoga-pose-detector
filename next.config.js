19eb83c3050d4459bf3203b252524c5a299bf76d
kkd6_ <kkd6_1999@yahoo.com>
2025-10-19 12:15:06 -0400

    Initial Setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Removed invalid 'appDir' option
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
